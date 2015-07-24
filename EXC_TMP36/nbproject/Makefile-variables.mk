421c3256c3f2daea3003ad6000eb5a4b35907148
mopus <hello@mopusworks.com>
2015-07-24 13:41:39 +0800

    Example program for the TMP36
    Works with similar Analog temperature sensors, or just analog sensors in
    general.
    This program includes calculation for converting raw ADC values to
    actual temperature according to the datasheet.

---
+++ EXC_TMP36/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=EXC_TMP36.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/EXC_TMP36.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=exctmp36.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/exctmp36.tar
